4ef33328cb4adff570c72a952607b8436cee75c5
Milan Rubbens <milan.rubbens7@gmail.com>
2023-01-31 15:32:30 +0100

    ExamenProgrammerenMilan

---
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,152 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(82, 44)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 32)
+        Me.Label1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(394, 263)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(150, 46)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Login"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(82, 66)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(200, 39)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(516, 66)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(200, 39)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(82, 31)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(122, 32)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Voornaam"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(516, 31)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(147, 32)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Wachtwoord"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(566, 263)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(150, 46)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Annuleren"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(82, 149)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(200, 39)
+        Me.TextBox3.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(82, 114)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(77, 32)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Naam"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(82, 260)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(286, 53)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "Encryptie Wachtwoord"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 32.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button3 As Button
+End Class
---
+++ WinFormsApp1/WinFormsApp1/Form1.vb
@@ -0,0 +1,86 @@
+﻿Imports System.Text.RegularExpressions
+
+Public Class Form1
+    Dim voornaam As String
+    Dim achternaam As String
+    Dim wachtwoord As String
+    Dim teller As Integer = 0
+    Dim username As String
+    Dim encryptie As Boolean = True
+    Dim encryptiewacht
+    Dim wachtwoord2
+    Dim lengtewachtwoord
+
+
+
+    Private Function foutePogingen(ByVal teller As Integer)
+        If teller >= 3 Then
+            Return MsgBox("Teveel foute pogingen")
+
+        End If
+        Return MsgBox("Probeer nog een keer")
+    End Function
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Hide()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        voornaam = TextBox1.Text
+        achternaam = TextBox3.Text
+        wachtwoord = TextBox2.Text
+        username = (voornaam & " " & achternaam)
+        If wachtwoord.Length < 8 Then
+            MsgBox("Het wachtwoord moet minstens 8 characters lang zijn")
+            TextBox2.Clear()
+            teller += 1
+            Call foutePogingen(teller)
+        ElseIf Not wachtwoord.Any(Function(c) Char.IsDigit(c)) Or (Regex.IsMatch(wachtwoord, "[~`!@#$%^&*()-+=|{}':;.,<>/?]")) Then
+            MsgBox("Het wachtwoord voldoet niet aan de eisen")
+            TextBox2.Clear()
+            teller += 1
+            Call foutePogingen(teller)
+        Else
+            MsgBox("Login was succesvol")
+            TextBox1.Hide()
+            TextBox2.Hide()
+            TextBox3.Hide()
+            Label3.Hide()
+            Button1.Hide()
+            Button3.Show()
+            Button2.Hide()
+
+            If encryptie Then
+                Label4.Text = ("Wachtwoord: " & "**********")
+                encryptie = False
+
+            Else
+                Label4.Text = ("Wachtwoord: " & wachtwoord)
+                encryptie = True
+            End If
+            Label2.Text = ("Username: " & username)
+
+        End If
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If encryptie Then
+            Label4.Text = "**********"
+            encryptie = False
+
+        Else
+            Label4.Text = wachtwoord
+            encryptie = True
+        End If
+    End Sub
+End Class
